46835121bc93bf2e8a0b82c9fb375c1ec1c81933
Francisco de Maussion <franciscodemaussion@gmail.com>
2021-08-09 22:31:57 -0300

    adds django base project

---
+++ cema_django/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cema_django.users.models import User
+from cema_django.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
